97281b6b44a2f4680354e68bbacf608c7d52dd09
Achnit Faissal <faissalachnit696@gmail.com>
2026-09-26 15:13:15 +0000

    function afficherListe function votes

--- main.js
+++ main.js
@@ -26,27 +26,112 @@ const candidats = [
     partiPolitique: "Indépendant",
     age: 40,
     electeurs: [],
-  },Z
+  },
 ];
-function AjouterNoueau() {
+function AjouterPlusieursCondidadts() {
   const candidat = {};
-  n = Number(p("Ajouter plusieurs candidats à la fois \t"));
+  n = Number(p("Combien de candidats voulez-vous ajouter ?"));
   for (let i = 0; i < n; i++) {
-    let cin = p("Enter le CIN \t");
-    let nom = p("Enter le Nom \t");
-    let prenom = p("Enter le prenom \t");
-    let partiPolitique = p("Ente le partiPlique \t");
-    let age = Number(p("Ente le age \t"));
-    let electeurs = [];
+    const candidat = {};
+    let cin = p("Enter le CIN  ");
+    let nom = p("Enter le Nom  ");
+    let prenom = p("Enter le prenom  ");
+    let partiPolitique = p("Ente le partiPlique  ");
+    let age = Number(p("Ente le age  "));
     candidat.cin = cin;
     candidat.nom = nom;
     candidat.prenom = prenom;
     candidat.partiPolitique = partiPolitique;
     candidat.age = age;
+    candidat.electeurs = [];
     candidats.push(candidat);
+
+    console.log("Candidat ajoute avec succes !\n");
   }
-  console.log(candidats);
 }
+
+function AfficherlisteCondidat() {
+  let choix = Number(
+    p(
+      "Choisissez une vue :\n" +
+        "1 - Trier les candidats par nombre de votes décroissant\n" +
+        "2 - Filtrer les candidats par parti politique\n",
+    ),
+  );
+
+  if (choix === 1) {
+    for (let i = 0; i < candidats.length - 1; i++) {
+      for (let j = 0; j < candidats.length - 1 - i; j++) {
+        if (candidats[j].electeurs.length < candidats[j + 1].electeurs.length) {
+          let temp = candidats[j];
+
+          candidats[j] = candidats[j + 1];
+
+          candidats[j + 1] = temp;
+        }
+      }
+    }
+
+    for (let i = 0; i < candidats.length; i++) {
+      console.log(`\n# Candidat ${i + 1}:`);
+      console.log(`CIN: ${candidats[i].cin}`);
+      console.log(`Nom: ${candidats[i].nom}`);
+      console.log(`Prénom: ${candidats[i].prenom}`);
+      console.log(`Parti politique: ${candidats[i].partiPolitique}`);
+      console.log(`Âge: ${candidats[i].age}`);
+      console.log(`Nombre de votes: ${candidats[i].electeurs.length}`);
+      console.log("-----------------------------");
+    }
+  } else if (choix === 2) {
+    let parti = p("Entrer le parti politique : ");
+
+    for (let i = 0; i < candidats.length; i++) {
+      if (candidats[i].partiPolitique === parti) {
+        console.log(`\n# Candidat ${i + 1}:`);
+        console.log(`CIN: ${candidats[i].cin}`);
+        console.log(`Nom: ${candidats[i].nom}`);
+        console.log(`Prénom: ${candidats[i].prenom}`);
+        console.log(`Parti politique: ${candidats[i].partiPolitique}`);
+        console.log(`Âge: ${candidats[i].age}`);
+        console.log(`Nombre de votes: ${candidats[i].electeurs.length}`);
+        console.log("-----------------------------");
+      }
+    }
+  } else {
+    console.log("Choix invalide. Veuillez entrer 1 ou 2.");
+  }
+}
+
+function VotesCondidats() {
+  let n = p("Donner voter CIN : ");
+  let dejaVote = false;
+
+  for (let i = 0; i < candidats.length; i++) {
+    for (let j = 0; j < candidats[i].electeurs.length; j++) {
+      if (n === candidats[i].electeurs[j]) {
+        dejaVote = true;
+      }
+    }
+  }
+  if (dejaVote) {
+    console.log("Vous avez deja vote");
+    return;
+  }
+  let p = p("Enter la CIN du candidat: ");
+  let trouve = false;
+  for (let i = 0; i < candidats.length; i++) {
+    if (candidats[i].cin === p) {
+      candidats[i].electeurs.push(p);
+      trouve = true;
+    }
+  }
+  if (trouve) {
+    console.log("Vote enregistre avec succes.");
+  } else {
+    console.log("Condidat introuvable.");
+  }
+}
+
 
 
 do {
@@ -55,10 +140,10 @@ do {
 
   switch (choix) {
     case 1:
-      AjouterNoueau();
+      AjouterPlusieursCondidadts();
       break;
     case 2:
-      console.log("2");
+      AfficherlisteCondidat();
       break;
     case 3:
       console.log("3");
@@ -87,4 +172,4 @@ do {
   }
 } while (choix !== 0);
 
-//   console.log("Quites");
+console.log("Quites");
